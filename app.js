cce081eda697ff7794479ede2ae4bdf25dd2cb2d
alamalcantara <alamalcantara99@gmail.com>
2020-07-13 14:21:34 -0400

    feat: first commit rest api with socket io

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const path = require('path');
+
+const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
+
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const {MONGODB_URI} = require('./config');
+const multer = require('multer');
+
+const app = express();
+
+const fileStorage = multer.diskStorage({
+    destination : (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename : (req, file, cb) => {
+        cb(null, `${new Date().toISOString()}-${file.originalname}`);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if(file.mimetype === 'image/png' 
+    || file.mimetype === 'image/jpg' 
+    || file.mimetype === 'image/jpeg') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
+app.use(bodyParser.json()); // application/json
+app.use(multer({storage: fileStorage, fileFilter}).single('image'));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
+
+
+app.use((error, req, res, next) => {
+    console.log(error);
+
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+
+    res.status(status).json({message, data});
+})
+
+mongoose.connect(MONGODB_URI)
+    .then(result => {
+        console.log('CONNECTED!');
+        const server = app.listen(8080);
+
+        const io = require('./socket').init(server);
+
+        io.on('connection', socket => {
+            console.log('CLIENT CONNECTED');
+        });
+    })
+    .catch(err => {
+        console.log(err);
+    })
